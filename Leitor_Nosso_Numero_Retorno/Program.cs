570b09d334ceff968494b7e1d3157af6e803668e
João Neto <jmdneto@gmail.com>
2018-02-16 13:09:06 -0200

    Adicionar arquivos de projeto.

---
+++ Leitor_Nosso_Numero_Retorno/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leitor_Nosso_Numero_Retorno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
